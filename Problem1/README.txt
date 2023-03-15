deb4cdc5d88bdd5d71bf40c94bdbce7c107b3b78
Ege Doğukan Seven <eseven20@ku.edu.tr>
2023-03-15 20:36:08 +0300

    Problem 1 Part (b)

--- Problem1/README.txt
+++ Problem1/README.txt
@@ -1,3 +1,8 @@
 To run problem 1 part a:
 gcc -o assignment1_p1a assignment1_p1a.c to compile,
 ./assignment1_p1a <argument n> to run the program.
+
+To run problem 2 part b:
+gcc -o assignment1_p1b assignment1_p1b.c to compile,
+./assignment1_p1b & to run the program background,
+ps -l to see current processes.
---
+++ Problem1/assignment1_p1b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+    
+    if(pid < 0) {
+        printf("Fork failed!");
+        exit(1);
+    }
+    else if(pid == 0) {
+        printf("Parent PID: %d, Child PID: %d\n", getppid(), getpid());
+        exit(0);
+    }
+    else {
+        sleep(10);
+        wait(NULL);
+    }
+
+    return 0;
+}
